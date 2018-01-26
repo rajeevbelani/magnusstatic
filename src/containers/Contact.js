3bd0b9914e9e3badb645a44feb392add1e20bade
Rajeev Belani <rajeev@woodsofttechnologies.com>
2018-01-26 10:01:27 +0530

    Added Contact Us

--- src/containers/Contact.js
+++ src/containers/Contact.js
@@ -14,69 +14,69 @@ import { getRouteProps, Link } from 'react-static'
 
 export default () => (
   <Box>
-      <Section pad="none" align="center" colorIndex="light-2">
-        <Box
-          size={{ width: 'xxlarge' }}
-          pad={{ horizontal: 'large', vertical: 'medium' }}
-        >
-    <Form>
-      {/* <Heading strong align="center">
+    <Section pad="none" align="center" colorIndex="light-2">
+      <Box
+        size={{ width: 'xxlarge' }}
+        pad={{ horizontal: 'large', vertical: 'medium' }}
+      >
+        <Form>
+          {/* <Heading strong align="center">
         RyanCollins.io
       </Heading> */}
-      <Heading align="center" tag="h5">
+          <Heading align="center" tag="h5">
         Contact Form
-      </Heading>
-      <FormFields>
-        <FormField
-          help="First and last name"
-          label="Name *"
-          htmlFor="nameInput"
-        >
-          <input
-            required
-            autoFocus
-            placeholder="Bill Clinton"
-            id="nameInput"
-            autoComplete="on"
-            name="name"
-            type="text"
-          />
-        </FormField>
-        <FormField
-          help="How should we get in touch with you?"
-          label="Email *"
-          htmlFor="emailInput"
-        >
-          <input
-            required
-            id="emailInput"
-            autoComplete="on"
-            name="email"
-            placeholder="bill@clinton.com"
-            type="email"
-          />
-        </FormField>
+          </Heading>
+          <FormFields>
+            <FormField
+              help="First and last name"
+              label="Name *"
+              htmlFor="nameInput"
+            >
+              <input
+                required
+                autoFocus
+                placeholder="Bill Clinton"
+                id="nameInput"
+                autoComplete="on"
+                name="name"
+                type="text"
+              />
+            </FormField>
+            <FormField
+              help="How should we get in touch with you?"
+              label="Email *"
+              htmlFor="emailInput"
+            >
+              <input
+                required
+                id="emailInput"
+                autoComplete="on"
+                name="email"
+                placeholder="bill@clinton.com"
+                type="email"
+              />
+            </FormField>
 
-      </FormFields>
-      <FormField
-        label="Message"
-        htmlFor="message-input"
-      >
-        <TextInput
-          required
-          id="message-input"
-        />
-      </FormField>
-      <Footer pad={{ vertical: 'medium' }} align="center">
-        <Button
-          fill
-          label="Submit"
-          primary
-        />
-      </Footer>
-    </Form>
-    </Box>
+          </FormFields>
+          <FormField
+            label="Message"
+            htmlFor="message-input"
+          >
+            <TextInput
+              required
+              id="message-input"
+            />
+          </FormField>
+          <Footer pad={{ vertical: 'medium' }} align="center">
+            <Button
+              fill
+              label="Submit"
+              primary
+            />
+          </Footer>
+        </Form>
+      </Box>
     </Section>
-    
+
   </Box>
 )
