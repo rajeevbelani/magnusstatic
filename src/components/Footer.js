bc7ee2e93ca0374cd5dae0d029060f9102faae02
Rajeev Belani <rajeev@woodsofttechnologies.com>
2018-02-23 02:39:55 +0530

    Styling changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,52 +9,49 @@ import { Facebook } from 'grommet-icons'
 import Menu from 'grommet/components/Menu'
 
 export default () => (
-  <Footer justify="between"
-    size="medium">
-    <Box direction="row"
-      align="center"
-      pad={{ between: 'medium' }}>
-      <Paragraph margin="none">
-      © 2018 Magnus Medi 
-      </Paragraph>
-      <Menu direction="row"
-        size="small"
-        dropAlign={{ right: 'right' }}>
-        <Anchor href="#">
-        Support
-        </Anchor>
-        <Anchor href="#">
-        Contact
-        </Anchor>
-        <Anchor href="#">
-        About
-        </Anchor>
-      </Menu>
-    </Box>
-  </Footer>
-  // <Footer colorIndex="neutral-4-t" justify="end" size="medium">
+  // <Footer justify="between"
+  //   size="medium">
   //   <Box direction="row"
+  //     align="center"
   //     pad={{ between: 'medium' }}>
   //     <Paragraph margin="none">
-  //       © 2018 Magnus Medi
+  //     © 2018 Magnus Medi 
   //     </Paragraph>
   //     <Menu direction="row"
   //       size="small"
   //       dropAlign={{ right: 'right' }}>
-  //       <Anchor href="http://www.magnusmedi.com/privacypolicy.html">
-  //        Privacy Policy
+  //       <Anchor href="#">
+  //       Privacy
   //       </Anchor>
-  //       <Anchor href="http://www.magnusmedi.com/privacypolicy.html">
-  //         Terms of Use
+  //       <Anchor href="#">
+  //       About
   //       </Anchor>
   //     </Menu>
-  //     <Box direction="row" align="end" responsive={false}>
-  //       <Facebook color="plain" />
-  //       <SocialShare type="twitter" link="http://bit.ly/2CvIP0H" />
-  //       <SocialShare type="facebook" link="http://bit.ly/2FdbHxw" />
-  //       <SocialShare type="linkedin" link="http://bit.ly/2EF0I36" />
-  //     </Box>
   //   </Box>
   // </Footer>
+  <Footer colorIndex="neutral-4-t" justify="end" size="medium">
+    <Box direction="row"
+      pad={{ between: 'medium' }}>
+      <Paragraph margin="none">
+        © 2018 Magnus Medi
+      </Paragraph>
+      <Menu direction="row"
+        size="small"
+        dropAlign={{ right: 'right' }}>
+        <Anchor href="http://www.magnusmedi.com/privacypolicy.html">
+         Privacy Policy
+        </Anchor>
+        <Anchor href="http://www.magnusmedi.com/privacypolicy.html">
+          Terms of Use
+        </Anchor>
+      </Menu>
+      <Box direction="row" align="end" responsive={false}>
+        
+        <SocialShare type="twitter" link="http://bit.ly/2CvIP0H" />
+        <SocialShare type="facebook" link="http://bit.ly/2FdbHxw" />
+        <SocialShare type="linkedin" link="http://bit.ly/2EF0I36" />
+      </Box>
+    </Box>
+  </Footer>
 )
 
