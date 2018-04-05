1792db9c2f1d0634dffcebaad6594e57579d1ef0
Rajeev Belani <rajeev@woodsofttechnologies.com>
2018-04-06 00:45:39 +0530

    Added Packages

--- static.config.js
+++ static.config.js
@@ -180,52 +180,11 @@ export default {
         <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
       </Head>
       <Body className="slug-home">
-        <script
-          dangerouslySetInnerHTML={{ __html: `
-        window.fbAsyncInit = function() {
-          FB.init({
-            appId      : '563161330727480',
-            xfbml      : true,
-            version    : 'v2.12'
-          });
         
-          FB.AppEvents.logPageView();
-        
-        };
-      
-        (function(d, s, id){
-          var js, fjs = d.getElementsByTagName(s)[0];
-          if (d.getElementById(id)) {return;}
-          js = d.createElement(s); js.id = id;
-          js.src = "https://connect.facebook.net/en_US/sdk.js";
-          fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk'));
-            ` }}
-        />
         {console.log(`children :: ${JSON.stringify(children)}`)}
         {children}
         {renderMeta.scripts &&
           renderMeta.scripts.map(script => <script type="text/javascript" src={`/${script}`} />)}
-        {/* <script>
-          window.fbAsyncInit = function() {
-            FB.init({
-              appId      : '563161330727480',
-              cookie     : true,
-              xfbml      : true,
-              version    : '{latest-api-version}'
-            })
-            FB.AppEvents.logPageView()
-
-          }
-
-          (function(d, s, id){
-            var js, fjs = d.getElementsByTagName(s)[0];
-            if (d.getElementById(id)) {return;}
-            js = d.createElement(s); js.id = id;
-            js.src = "https://connect.facebook.net/en_US/sdk.js";
-            fjs.parentNode.insertBefore(js, fjs);
-          }(document, 'script', 'facebook-jssdk'));
-        </script> */}
       </Body>
     </Html>
   ),
